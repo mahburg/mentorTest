6a9207f0f009f531d7f4e74180263009b0bd4ecb
Lloyd Grubham <grubham@gmail.com>
2017-06-20 00:01:58 -0600

    First commit for completion.

--- toyProblems.js
+++ toyProblems.js
@@ -1,27 +1,77 @@
 //JS Basics
 //Run npm test in the command line to test your solutions
 module.exports = {
-  // reverseIt:
+  reverseIt: function (strIn) {
+    return strIn.split('').reverse().join('');
+  },
 
   //uncomment and finish the reverseIt function. It will take in one parameter which is a String and
   //reverse it
 
-  // removeDups:
+  removeDups: function (arr) {
+    let itBe = [];
+    for (let i = 0; i < arr.length; i++) {
+      let temp = true;
+      for (let j = 0; j < itBe.length; j++) {
+        if (arr[i] === itBe[j]){
+          temp = false;
+        }
+      }
+      if (temp){
+        itBe.push(arr[i]);
+      }
+    }
+    return itBe;
+  },
+  //The above solution will not work for arrays containing arrays or objects.
 
   //uncomment and finish the removeDups function. It will take in one parameter which is an Array
   //remove all duplicates
 
-  // titleIt:
+  titleIt: function (strIn) {
+    let arr = strIn.split(' ');
+    for (let i = 0; i < arr.length; i++){
+      if(arr[i][0].toLowerCase() === arr[i][0]){
+        arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
+      }
+    }
+    return arr.join(' ');
+  },
 
   //uncomment and finish the titleIt function. It will take in one parameter which is a String and
   //capitalize the first letter of each word
 
-  // vowelCounter:
+  vowelCounter: function (strIn) {
+    let counter = 0;
+    for (var i = 0; i < strIn.length; i++) {
+      let j = strIn[i].toLowerCase();
+      if (j === 'a' || j === 'e' || j === 'i' || j === 'o' || j === 'u'){
+        counter++;
+      }      
+    }
+    return counter;
+  },
 
   //uncomment and finish the vowelCounter function. It will take in one parameter which is a String and
   //return the number of vowels in the string
 
-  // isPrime:
+  isPrime: function (n) {
+    if (n < 2){
+      return false;
+    }
+    if (n === 2){
+      return true;
+    }
+    if (n % 2 === 0){
+      return false;
+    }
+    for (let i = 3; i < (n/2); i += 2){
+      if (n % i === 0){
+        return false;
+      }
+    }
+    return true;
+  },
 
   //uncomment and finish the isPrime function. It will take in one parameter which is a Number and
   //return true if it is prime and false if it is not
@@ -30,7 +80,7 @@ module.exports = {
   //var foo = 10 + '20';
   //uncomment the foo property and give your answer as its value
 
-  //foo:
+  foo: '1020',
 
   //what is the outcome of the two console.logs below?
   //   var foo = "Hello";
@@ -41,6 +91,6 @@ module.exports = {
   // console.log(foo + bar);
   // uncomment the log1 and log2 properties and give your answers as their values
 
-  // log1:
-  // log2:
+  log1: "Hello World",
+  log2: undefined
 }
